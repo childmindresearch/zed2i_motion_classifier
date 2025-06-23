39cc1791e8655e7d782b17dc9609eb992bd041c8
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-06-23 14:42:04 -0400

    Update __main__.py
    manual updating from moira

--- __main__.py
+++ __main__.py
@@ -2,11 +2,5 @@
 
 from core import cli
 
-
-def run_main() -> None:
-    """Main entry point to mobi_motion_tracking."""
-    cli.main()
-
-
 if __name__ == "__main__":
-    run_main()
+    cli.main()
